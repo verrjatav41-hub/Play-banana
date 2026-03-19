1694c70bdc88175d10fed35a789f8d9d5e28160b
verrjatav41-hub <verrjatav41@gmail.com>
2026-03-19 15:10:46 +0530

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,14 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.playbanana.rewards.app',
+  appName: 'Play Banana',
+  webDir: 'dist',
+  plugins: {
+    AdMob: {
+      androidAppId: 'ca-app-pub-3940256099942544~3347511713', // Test App ID
+    },
+  },
+};
+
+export default config;
